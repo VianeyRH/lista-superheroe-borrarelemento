ee576f3057cf2de3fd0406f74dbaf8479512276c
VianeyRH <201821898@tese.edu.mx>
2021-04-19 22:22:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lista-superheroe-borrarelemento
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/lista-superheroe-borrarelemento)
---
+++ src/app/listado/listado.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent{
+  heroes: string[] = ['Hulk', 'Saitama', 'Superman', 'Thor', 'Goku'];
+  elim: string="";
+
+  elimHero(): void{
+    this.elim = this.heroes.pop();
+  }
+  mostrar():string{
+    if(this.elim!="")
+      return this.elim+"Elemento Borrado";
+  }
+}
